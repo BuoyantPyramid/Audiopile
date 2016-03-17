3c494c147a92628c3618596f19193c6fdd29b8ad
Erick Paepke <epaepke@gmail.com>
2016-03-17 12:06:37 -0700

    (test) Add test to confirm user sign up

--- test/client/e2e/login.spec.js
+++ test/client/e2e/login.spec.js
@@ -1,9 +1,26 @@
+// Replace this later
+var userEmail = 'buoyantpyramid2@gmail.com';
+
 describe('Login processes', function() {
-  describe("index", function() {
+  browser.get('http://localhost:3000');
+
+  describe('index', function() {
     it('should display correct title', function() {
-      browser.get('http://localhost:3000');
       expect(browser.getTitle()).toEqual('Jam Record');
     });
-    
-  })
+
+    it('allow new users to signup', function() {
+      var email = element(by.model('user.email'));
+      var pass = element(by.model('user.password'));
+      var signupButton = element(by.id('signup'));
+
+      email.sendKeys(userEmail);
+      pass.sendKeys('password');
+
+      signupButton.click();
+
+      var header = element(by.css('#profile > h2'))
+      expect(header.getText()).toEqual('Profile');
+    });
+  });
 });
