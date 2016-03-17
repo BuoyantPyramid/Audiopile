9d9d4dd76e42fa40a48d85fa228e16f986dee371
Erick Paepke <epaepke@gmail.com>
2016-03-17 12:06:37 -0700

    (test) Add test to confirm user sign up

--- test/client/e2e/conf.js
+++ test/client/e2e/conf.js
@@ -12,7 +12,7 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['services-spec.js'],
+  specs: ['login.spec.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
